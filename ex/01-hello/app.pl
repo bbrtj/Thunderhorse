f37499632f22342f812deb44ef7f3c57d7a3dcc4
bbrtj <bbrtj.pro@gmail.com>
2025-12-30 21:46:14 +0100

    Example 01: do not use block package syntax

--- ex/01-hello/app.pl
+++ ex/01-hello/app.pl
@@ -1,24 +1,24 @@
 use v5.40;
 
-package HelloApp {
-	use Mooish::Base -standard;
-	extends 'Thunderhorse::App';
+package HelloApp;
 
-	sub build ($self)
-	{
-		my $r = $self->router;
+use Mooish::Base -standard;
+extends 'Thunderhorse::App';
 
-		$r->add(
-			'/hello/?msg' => {
-				to => sub ($self, $ctx, $msg) {
-					return "Hello, $msg";
-				},
-				defaults => {
-					msg => 'world',
-				},
-			}
-		);
-	}
+sub build ($self)
+{
+	my $r = $self->router;
+
+	$r->add(
+		'/hello/?msg' => {
+			to => sub ($self, $ctx, $msg) {
+				return "Hello, $msg";
+			},
+			defaults => {
+				msg => 'world',
+			},
+		}
+	);
 }
 
 HelloApp->new->run;
